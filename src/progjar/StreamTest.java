3e60597fd21adcdb6dc160da84163096cf005310
ramamimu <ahhmaad.blogger@gmail.com>
2023-05-03 10:47:43 +0700

    feat: succesfully encrypted using SSL

--- src/progjar/StreamTest.java
+++ src/progjar/StreamTest.java
@@ -4,12 +4,17 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.Socket;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 
 public class StreamTest {
 	public static void main(String[] args) {
-		try {			
-			Socket socket = new Socket("my.its.ac.id", 443);
+		try {
+			SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+			SSLSocket socket = (SSLSocket) factory.createSocket("my.its.ac.id", 443);
+
+			// Socket socket = new Socket("my.its.ac.id", 443);
 			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
